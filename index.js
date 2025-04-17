58c9aaf602329444b932c9fbeec117a9b05c5b2f
Hồ Đình Trí <162857265+trib2203591@users.noreply.github.com>
2025-04-17 17:18:59 +0700

    fix issue relate to cors and crc32 something

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 import { getUploadPresignedURL } from './write.js';
 
+
+const s3 = new S3Client({
+  endpoint: "http://localhost:3900",
+  region: "garage",
+  credentials: {
+      accessKeyId: "GK0a637988d31fb3dcf44f7eea",
+      secretAccessKey: "64641c8dce3bbd73c14cf16a0a6ba4b71e263a65e051005ded6ebed9ed60433e"
+  },
+  forcePathStyle: true
+});
+
 const app = express();
 const PORT = 3000;
 
